2f4bec9e58968fe1d97c1c9eca8ea987fdd8b405
James Leskovar <jleskovar@gmail.com>
2017-12-26 10:55:09 +1100

    async websocket example using rxjava to walk the blockchain

--- src/main/kotlin/com/github/jleskovar/btcrpc/examples/RxApi.kt
+++ src/main/kotlin/com/github/jleskovar/btcrpc/examples/RxApi.kt
@@ -5,30 +5,49 @@ import com.github.jleskovar.btcrpc.BitcoinRpcClientFactory
 import com.github.jleskovar.btcrpc.BlockInfoWithTransactions
 import com.github.jleskovar.btcrpc.Transaction
 import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Created by james on 25/12/17.
  */
-class BlockWalker(val client: AsyncBitcoinRpcClient) {
+class BlockWalker(val client: AsyncBitcoinRpcClient, val numWorkers: Int = 8) {
+    private val workCount = AtomicInteger(0)
+
     fun getBlocks(): Observable<BlockInfoWithTransactions> {
-        return Observable
-                .fromFuture(client.getBlockchainInfo())
-                .flatMap { Observable.range(0, it.blocks!!.toInt()) }
-                .flatMap { Observable.fromFuture(client.getBlockHash(it)) }
-                .flatMap { Observable.fromFuture(client.btcdGetBlockWithTransactions(it)) }
+        return adviseObservable( Observable
+            // Defer the future to allow Rx to schedule onto IO pool
+            .defer { Observable.fromFuture(client.getBlockchainInfo()) }
+            .flatMap { Observable.range(0, it.blocks!!.toInt()) }
+            .flatMap { Observable.fromFuture(client.getBlockHash(it)) }
+            .flatMap { Observable.fromFuture(client.btcdGetBlockWithTransactions(it)) }
+        )
     }
 
     fun getTransactions(): Observable<Transaction> {
         return getBlocks().flatMap { Observable.fromIterable(it.rawtx) }
     }
+
+    private fun <T : Any?> adviseObservable(o: Observable<T>): Observable<T> {
+        return o.subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .groupBy({ workCount.getAndIncrement() % numWorkers })
+                .flatMap { it.observeOn(Schedulers.computation()) }
+    }
 }
 
 fun main(args: Array<String>) {
     val webSocketClient = BitcoinRpcClientFactory.createAsyncWsClient("james", "james", "localhost", 18334, true)
+
     webSocketClient.connect()
 
-    val blockWalker = BlockWalker(webSocketClient)
-    blockWalker.getTransactions().subscribe { println(it.hash) }
+    val blockWalker = BlockWalker(webSocketClient, numWorkers = 2)
+
+    blockWalker
+            .getTransactions()
+            .subscribe { println("THR[${Thread.currentThread().name}] ${it.hash} - ${it.confirmations}") }
+
+    Thread.sleep(1000)
 
     webSocketClient.disconnect()
 }
